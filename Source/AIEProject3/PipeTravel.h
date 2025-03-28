50ccafe42c70b9d5c4fafffa0af81ec98ebecea7
Charlie Campbell <chacha9669@hotmail.com>
2025-03-28 12:23:32 +1100

    finished pipe travelling
    currently only works 1 way, lmk if you need two way

--- Source/AIEProject3/PipeTravel.h
+++ Source/AIEProject3/PipeTravel.h
@@ -16,7 +16,39 @@ class AIEPROJECT3_API APipeTravel : public ATriggerSphere
 
 public:
 
+	APipeTravel();
+
+	virtual void Tick(float DeltaTime) override;
+
 	//Called by blueprint when a player enters the zone
 	UFUNCTION(BlueprintCallable)
-	void StartTravelling();
+	void StartTravelling(AActor* InCharacter);
+
+	//Implemented in blueprints, gives player control again
+	UFUNCTION(BlueprintImplementableEvent)
+	void Eject(AActor* InCharacter, FVector ForceDirection, float Force);
+
+	//Stores the 'keyframes' the player needs to travel to along the pipe
+	UPROPERTY(EditInstanceOnly)
+	TArray<class ATargetPoint*> Targets;
+
+	//Units per second the hampter will travel through the pipe
+	UPROPERTY(EditInstanceOnly)
+	float TravelSpeed = 1000.0f;
+
+	UPROPERTY(EditInstanceOnly)
+	float EjectForce = 1.0f;
+
+private:
+	//runs when travelling
+	void Travel(float DeltaTime);
+
+	//Stores a pointer to the player that we are trying to move
+	AActor* Player;
+
+	//When true, the script will move the player through the pipe
+	bool bIsTravelling;
+
+	//index to the target the player should be moving to
+	int targIndex = 0;
 };
